c7fe7e28bd1dcad9786400d10889f443752a6733
Nazar Zubyk <kukurudza.v.tanke@gmail.com>
2024-05-24 15:22:28 +0300

    connect + yml update

--- src/db_API/connect.ts
+++ src/db_API/connect.ts
@@ -32,10 +32,11 @@ const connectWithRetry = async (config: DbConfig, retries = 5, delay = 5000): Pr
 };
 
 const dbConfig: DbConfig = {
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'db',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '88888888',
+  password: process.env.DB_PASSWORD || '111111112',
   database: process.env.DB_NAME || 'myDB'
 };
 
 export const connection = connectWithRetry(dbConfig);
+
